5282683572caf642c10bf15a286b2024720359c8
Bhavesh <bhaveshpraveen10@gmail.com>
2017-10-28 02:52:29 +0530

    Datetime Field in adminTemplate

--- QandA/models.py
+++ QandA/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class QuestionAndAnswers(models.Model):
     user = models.CharField(max_length=125, help_text='Enter your User Name')
     answer = models.TextField(help_text='Your answer goes here')
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return "{} - {}".format(self.user, self.answer)
+        return "{} - {} - {}:{}:{}".format(self.user, self.answer, self.updated.hour, self.updated.minute, self.updated.second)
